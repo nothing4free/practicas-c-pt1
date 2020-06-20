7ad4598f2412bcf155d498002f7d3155f911d445
Jorge Martínez Hurtado <45176388+jorgemhdev@users.noreply.github.com>
2020-06-20 16:46:54 +0200

    Rename ej2.c to 2-MCD.c

--- 2-MCD.c
+++ 2-MCD.c
@@ -7,7 +7,7 @@
    Este programa halla el MCD (maximo comun divisor) de dos numeros, y lo muestra por pantalla.
    
    Por comodidad a la hora de interpretar el codigo, divido su funcionamiento en funciones. 
-   A mi parecer, así se entiende mejor el funcionamiento del programa.
+   A mi parecer, asÃ­ se entiende mejor el funcionamiento del programa.
  -----------------------------------------------------------------------------------------------------------------
 */
 
