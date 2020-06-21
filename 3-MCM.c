4292d7d672cd887b1ae387110dd50c0f2b64375c
Jorge Martínez Hurtado <45176388+jorgemhdev@users.noreply.github.com>
2020-06-21 12:41:20 +0200

    fix: uploaded correct file

--- 3-MCM.c
+++ 3-MCM.c
@@ -1,32 +1,45 @@
-#include <stdio.h>
+/* 
+ -----------------------------------------------------------------------------------------------------------------
+   DISCLAIMER:
+   Este codigo puede ser reutilizado, redistribuido y/o modificado siempre que se de credito a su autor original:
+   Jorge Martinez Hurtado (jorgemhdev) | 2020
 
-/*
-FUNCIONAMIENTO DEL PROGRAMA
-	1. se piden los dos numeros por pantalla
-	2. se descompone factorialmente el primero, y se almacenan sus factores en un array.
-	3. se descompone factorialmente el segundo, y se almacenan sus factores en un array.
-	4. se multiplican los dos arrays y se guarda el resultado.
-	5. se muestra en pantalla dicho resultado: el MCM. 
+   Este programa halla el MCM (minimo comun multiplo) de dos numeros (a, b) y lo muestra por pantalla.
+   
+   Esta es una de muchas formas que hay de realizar este ejercicio. Por simplificarlo, he aplicado la siguiente
+   fórmula que se puede encontrar en https://es.wikipedia.org/wiki/Minimo_comun_multiplo :
+   
+   				 a * b
+   MCM(a, b) = ---------
+   			   MCD(a, b)
+   			   
+   Aprovechando que en un ejercicio anterior había realizado un programa que calculaba el maximo comun divisor
+   de dos numeros, he implementado codigo de dicho ejercicio en este programa.
+   
+   Las funciones pideNumero(), calculaDiv1(), calculaDiv2() y calculaMCD() se corresponden al ejercicio
+   "2-MCD.c", disponible en el siguiente repositorio de GitHub: https://github.com/jorgemhdev/practicas-c-pt1
+   
+   Por comodidad a la hora de interpretar el codigo, divido su funcionamiento en funciones.
+   A mi parecer, así se entiende mejor el funcionamiento del programa.
+ -----------------------------------------------------------------------------------------------------------------
 */
 
+#include <stdio.h>
+
+//declaramos variables globales que recogen la entrada de usuario.
 int numero1;
 int numero2;
-int factoresTotales[100];
 
-typedef struct{
-	
-	int base;
-	int exponente;
-	
-}factores_t;
+//declaramos arrays globales en los que almacenaremos los divisores de los dos numeros.
+int divisores1[100];
+int divisores2[100];
 
-factores_t factores1[100];
-factores_t factores2[100];
-factores_t factoresFinal[100];
-
-int MCM;
+//Primero, calculamos el MC
+int MCD;
 
 void pideNumero(){
+	
+	//la funcion pideNumero obtiene los dos numeros por entrada de usuario por teclado
 	
 	printf("Introduce el numero 1...\n");
 	scanf("%d", &numero1);
@@ -38,259 +51,151 @@ void pideNumero(){
 	
 }
 
-void factoriza1(){
+void calculaDiv1(){
 	
-	int contador1 = 0; //contador del array estructura
-	int divisor1 = 2; //empieza en 2 porque 1 es factor de todos los numeros, asi que lo obviamos
-
-	while(divisor1 <= numero1){
+	//la funcion calculaDiv1 obtiene los divisores del primer numero introducido.
+	
+	int i = 1;
+	int contadorDivisor1 = 0;
+	
+	while(i <= numero1){
 		
-		
-		if((numero1 % divisor1) == 0){
+		if((numero1 % i) == 0){
 			
-			factores1[contador1].base = divisor1;
-			factores1[contador1].exponente++;
-			numero1 = (numero1 / divisor1);
+			divisores1[contadorDivisor1] = i;
+			contadorDivisor1++;
+			i++;
 			
 		}
-		
 		else{
-			
-			divisor1++;
-			
-			if(factores1[contador1].base != '\0'){
-				
-				contador1++;
-			
-			}
-
-			
+	
+			i++;
+	
 		}
-		
+	
 	}
 
-/*
-	int imprime = 0;
-	
-	while(factores1[imprime].base != '\0'){
+/*  
+	DESCOMENTAR PARA PROBAR LA FUNCION INDIVIDUALMENTE: imprime los divisores del numero 1
+	divisores1[contadorDivisor1+1] = '\0';
+	int k = 0;
+	while(divisores1[k] != '\0'){
 		
-		printf("Base: %d\n", factores1[imprime].base);
-		printf("Exponente: %d\n", factores1[imprime].exponente);
-		printf("-----------------\n");
-		imprime++;
+		printf("%d\n", divisores1[k]);
+		k++;
 		
 	}
 */
 
 }
 
-void factoriza2(){
-
-	int contador2 = 0; //contador del array estructura
-	int divisor2 = 2; //empieza en 2 porque 1 es factor de todos los numeros, asi que lo obviamos
-
-	while(divisor2 <= numero2){
-		
-		
-		if((numero2 % divisor2) == 0){
-			
-			factores2[contador2].base = divisor2;
-			factores2[contador2].exponente++;
-			numero2 = (numero2 / divisor2);
-			
-		}
-		
-		else{
-			
-			divisor2++;
-			
-			if(factores2[contador2].base != '\0'){
-				
-				contador2++;
-			
-			}
-
-			
-		}
-		
-	}
-
-/*
-	int imprime = 0;
+void calculaDiv2(){
 	
-	while(factores2[imprime].base != '\0'){
+	//la funcion calculaDiv1 obtiene los divisores del segundo numero introducido.
+	
+	int j = 1;
+	int contadorDivisor2 = 0;
+	
+	while(j <= numero2){
 		
-		printf("Base: %d\n", factores2[imprime].base);
-		printf("Exponente: %d\n", factores2[imprime].exponente);
-		printf("-----------------\n");
-		imprime++;
+		if((numero2 % j) == 0){
+			
+			divisores2[contadorDivisor2] = j;
+			contadorDivisor2++;
+			j++;
+			
+		}
+		else{
+	
+			j++;
+	
+		}
+	
+	}	
+	
+/* 
+	DESCOMENTAR PARA PROBAR LA FUNCION INDIVIDUALMENTE: imprime los divisores del numero 2
+	divisores2[contadorDivisor2+1] = '\0';
+	int k = 0;
+	while(divisores2[k] != '\0'){
 		
-	}
-*/
+		printf("%d\n", divisores2[k]);
+		k++;
+		
+	}	
+*/	
+	
 }
 
-void guardaArray(){
+int calculaMCD(){
 	
-	int contador1 = 0; //recorre los arrays de factores
-	int contador2 = 0; //recorre el array de factores comunes y no comunes
+	//la funcion calculaMCD compara los divisores de ambos numeros y almacena en la variable MCD el mayor de ellos.
 	
+	//determinamos la cantidad de divisores del primer numero
 	int size1 = 0;
-	int size2 = 0;
-	
-	while(factores1[size1].base != '\0'){
+	while(divisores1[size1] != '\0'){
 		
 		size1++;
 		
 	}
 	
-	while(factores2[size2].base != '\0'){
-		
-		size2++;
-		
-	}
-
-	printf("size1 = %d\n", size1);
-	printf("size2 = %d\n", size2);
-	printf("--------------------------------\n");
+	//declaramos variables contador para los siguientes bucles
+	int contador1 = 0;
+	int contador2 = 0;
 	
-	if(size1 < size2){
+	/*
+	Los siguients bucles anidados comparan los arrays en los que se encuentran los divisores de los dos numeros. Su funcionamiento es el siguiente:
+	
+    Compara el primer divisor del primer numero con todos los divisores del segundo.		
+	 -> Si coinciden los valores, se actualiza el valor de la variable MCD y se pasa al segundo divisor del primer numero, repitiendose el mismo proceso.
+	 -> Si no coinciden los valores, se pasa al segundo divisor del primer numero, repitiendose el mismo proceso.
+	
+	Este proceso se repite hasta que se llega al caracter nulo en el array de los divisores del primer numero. En ese instante, la variable MCD
+	tendra el divisor comun mas alto, dado que los contadores de los bucles van aumentando su valor cada vez estos se ejecutan.
+	*/
 		
-		while(factores2[contador1].base != '\0'){
+	while(contador1 < size1){ //bucle que recorre el array de los divisores del primer numero...
 		
-			if(factores1[contador1].base == factores2[contador1].base){ //si las bases son iguales, compara los exponentes y guarda la base con el mayor exponente
+		while(divisores2[contador2] != '\0'){ // ... y lo compara con todos los divisores del segundo numero.
 			
-				if(factores1[contador1].exponente > factores2[contador1].exponente){
+			if(divisores1[contador1] == divisores2[contador2]){
 				
-					if(factores1[contador1].base != '\0'){ //solo se graba si son distintos de \0
-					
-						factoresFinal[contador2].base = factores1[contador1].base;
-						factoresFinal[contador2].exponente = factores1[contador1].exponente;
-						contador2++;								
+				MCD = divisores1[contador1]; // se actualiza el valor de la variable MCD con el que, en el momento, es el divisor comun mas alto.
+				contador2++;
 				
-					}
-				
-				}
-				else{ //si los exponentes son iguales o el segundo es mayor, se graba el segundo.
-				
-					if(factores1[contador1].base != '\0'){
-					
-						factoresFinal[contador2].base = factores2[contador1].base;
-						factoresFinal[contador2].exponente = factores2[contador1].exponente;
-						contador2++;					
-					
-					}
-
-				}
-			
-				contador1++;
-			
 			}
-		
-			else{ //CORREGIR ERROR
 			
-				if(factores1[contador1].base != '\0'){
+			else{
 				
-					factoresFinal[contador2].base = factores1[contador1].base;
-					factoresFinal[contador2].exponente = factores1[contador1].exponente;
-					contador2++;
+				contador2++;
 				
-				}
+			}
 			
-				if(factores2[contador1].base != '\0'){
-				
-					factoresFinal[contador2].base = factores2[contador1].base;
-					factoresFinal[contador2].exponente = factores2[contador1].exponente;
-					contador2++;				
-				
-				}
-		
-			contador1++;	
-		
-			}		
-		
 		}
-		
+	
+	contador1++;
+	contador2 = 0;
+	
 	}
 	
-	if(size2 < size1 || size1 == size2){
-
-		while(factores1[contador1].base != '\0'){
-		
-			if(factores1[contador1].base == factores2[contador1].base){ //si las bases son iguales, compara los exponentes y guarda la base con el mayor exponente
-			
-				if(factores1[contador1].exponente > factores2[contador1].exponente){
-				
-					if(factores1[contador1].base != '\0'){ //solo se graba si son distintos de \0
-					
-						factoresFinal[contador2].base = factores1[contador1].base;
-						factoresFinal[contador2].exponente = factores1[contador1].exponente;
-						contador2++;								
-				
-					}
-				
-				}
-				else{ //si los exponentes son iguales o el segundo es mayor, se graba el segundo.
-				
-					if(factores1[contador1].base != '\0'){
-					
-						factoresFinal[contador2].base = factores2[contador1].base;
-						factoresFinal[contador2].exponente = factores2[contador1].exponente;
-						contador2++;					
-					
-					}
-
-				}
-			
-				contador1++;
-			
-			}
-		
-			else{ //CORREGIR ERROR
-			
-				if(factores1[contador1].base != '\0'){
-				
-					factoresFinal[contador2].base = factores1[contador1].base;
-					factoresFinal[contador2].exponente = factores1[contador1].exponente;
-					contador2++;
-				
-				}
-			
-				if(factores2[contador1].base != '\0'){
-				
-					factoresFinal[contador2].base = factores2[contador1].base;
-					factoresFinal[contador2].exponente = factores2[contador1].exponente;
-					contador2++;				
-				
-				}
-		
-			contador1++;	
-		
-			}
-		
-		
-		}
-		
-		
-	}
-		
-	int imprime = 0;
-	
-	while(factoresFinal[imprime].base != '\0'){
-		
-		printf("Base: %d\n", factores2[imprime].base);
-		printf("Exponente: %d\n", factores2[imprime].exponente);
-		printf("-----------------\n");
-		imprime++;
-		
-	}	
+	//se devuelve el valor del MCD, ya que operaremos con este valor mas adelante.
+	return(MCD);
 
 }
+
 
 void main(){
 	
+	//primero hallamos el MCD
 	pideNumero();
-	factoriza1();
-	factoriza2();
-	//guardaArray();
+	calculaDiv1();
+	calculaDiv2();
+	
+	//a continuacion asignamos el valor del MCD  la variable denominador, y operamos con ella para aplicar la formula expuesta anteriormente.
+	int denominador = calculaMCD();
+	float MCM = ((numero1 * numero2) / denominador);
+	printf("El minimo comun multiplo de %d y %d es %f\n", numero1, numero2, MCM);
 	
 }
+
+//Jorge Martinez Hurtado (jorgemhdev) | 2020
