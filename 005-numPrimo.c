933bebab71e0682fed3b2778efb3277ffc5bbdd2
Jorge Martínez Hurtado <45176388+jorgemhdev@users.noreply.github.com>
2020-06-28 17:45:20 +0200

    file: renamed 5-numPrimo.c to 005-numPrimo.c

--- 005-numPrimo.c
+++ 005-numPrimo.c
@@ -7,7 +7,7 @@
    Este programa recibe un numero mediante entrada de usuario y determina si es primo o no.
    
    Por comodidad a la hora de interpretar el codigo, divido su funcionamiento en funciones. 
-   A mi parecer, así se entiende mejor el funcionamiento del programa.
+   A mi parecer, asÃ­ se entiende mejor el funcionamiento del programa.
  -----------------------------------------------------------------------------------------------------------------
 */
 
